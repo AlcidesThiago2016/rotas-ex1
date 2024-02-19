535db2c451c828eeb43df2492f10115f0468f663
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-02-19 16:05:09 -0300

    Layout

--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,8 +1,20 @@
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
 export default function HomeBody(){
 
     return(
         <main>
-            <h2>Pagina Inicial</h2>
+            <section>
+                <div className="container">
+                    <div className="mt30 mb30">
+                        <Card  title="Página Inicial"/>
+                    </div>
+                    <div className="dflex">
+                        <Button text="Ver Promoção"></Button>
+                    </div>  
+                </div>
+            </section>
         </main>
     )
 }
